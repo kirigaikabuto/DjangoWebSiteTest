4bcc1d18b27f06b21da859b346185df154dbb5fb
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-08-06 12:11:46 +0600

    ssdsd

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -107,3 +108,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
